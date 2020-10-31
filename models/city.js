ac92c37842f1d6bcb9ff8aa4a4eeecc2f71ec364
ricksubel <ricksubel@gmail.com>
2020-10-31 17:18:52 -0400

    commit before computer restart

--- models/city.js
+++ models/city.js
@@ -4,6 +4,10 @@ const Schema = mongoose.Schema;
 const CitySchema = new Schema({
     name: String,
     posts: String,
+    resources: 
+    [
+
+    ]
 });
 
 const City = mongoose.model('City', CitySchema);
